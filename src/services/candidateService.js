14a0de2f4c9588ef1677ffc6c3e78c74ac3517c7
GeminusF <78738210+GeminusF@users.noreply.github.com>
2021-06-11 00:52:26 +0400

    CandidateService all HTTP requests added

--- src/services/candidateService.js
+++ src/services/candidateService.js
@@ -1,6 +1,26 @@
 import axios from "axios";
 
 export default class CandidateService {
+  registerCandidate({
+    birthDate,
+    emailAddress,
+    firstName,
+    id,
+    identificationNumber,
+    lastName,
+    password,
+  }) {
+    return axios.post("http://localhost:8080/api/candidates/register", {
+      birthDate,
+      emailAddress,
+      firstName,
+      id,
+      identificationNumber,
+      lastName,
+      password,
+    });
+  }
+
   getCandidates() {
     return axios.get("http://localhost:8080/api/candidates/getall");
   }
