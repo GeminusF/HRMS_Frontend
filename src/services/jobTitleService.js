7070797ec95f39fad2f86bcd09a5639ea6a876e0
GeminusF <78738210+GeminusF@users.noreply.github.com>
2021-06-11 00:51:36 +0400

    JobTitleService all HTTP requests added

--- src/services/jobTitleService.js
+++ src/services/jobTitleService.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 
 export default class JobTitleService {
+  addJobTitle({ id, title }) {
+    return axios.post("http://localhost:8080/api/job_titles/add", {
+      id,
+      title,
+    });
+  }
+
   getJobTitles() {
     return axios.get("http://localhost:8080/api/job_titles/getall");
   }
