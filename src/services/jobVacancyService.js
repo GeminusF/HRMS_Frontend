9c4aaaccd940b051c4df9ad02539456c43c6f303
GeminusF <78738210+GeminusF@users.noreply.github.com>
2021-06-11 00:43:10 +0400

    JobVacancyService all HTTP requests added

--- src/services/jobVacancyService.js
+++ src/services/jobVacancyService.js
@@ -1,7 +1,57 @@
 import axios from "axios";
 
 export default class JobVacancyService {
+
+  addJobVacancy({
+    active,
+    applicationDeadline,
+    cityInfo,
+    companyName,
+    id,
+    jobDescription,
+    jobPosition,
+    openPositionNumber,
+    releaseDate,
+    salaryMax,
+    salaryMin,
+  }) {
+    return axios.post("http://localhost:8080/api/jobvacancies/add", {
+      active,
+      applicationDeadline,
+      cityInfo,
+      companyName,
+      id,
+      jobDescription,
+      jobPosition,
+      openPositionNumber,
+      releaseDate,
+      salaryMax,
+      salaryMin,
+    });
+  }
+
   getJobVacancies() {
     return axios.get("http://localhost:8080/api/jobvacancies/getall");
   }
+
+  getAvctiveJobVacancies() {
+    return axios.get("http://localhost:8080/api/jobvacancies/isactive/active");
+  }
+
+  getAvctiveJobVacanciesOrderedByCompanyName() {
+    return axios.get(
+      "http://localhost:8080/api/jobvacancies/isactive/active/orderedbycompanyname"
+    );
+  }
+
+  getActiveJobVacanciesOrderedByReleaseDateAsc() {
+    return axios.get(
+      "http://localhost:8080/api/jobvacancies/isactive/active/orderedbyreleasedateasc"
+    );
+  }
+
+  getPassiveJobVacancies() {
+    return axios.get("http://localhost:8080/api/jobvacancies/isactive/passive");
+  }
+
 }
