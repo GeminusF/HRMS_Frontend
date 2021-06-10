3a84675781da2a274cbc8714f88ab85f35364d8b
GeminusF <78738210+GeminusF@users.noreply.github.com>
2021-06-11 00:52:11 +0400

    EmployerService all HTTP requests added

--- src/services/employerService.js
+++ src/services/employerService.js
@@ -1,6 +1,16 @@
 import axios from "axios";
 
 export default class EmployerService {
+  registerEmployer({ companyName, emailAddress, id, password, webAddress }) {
+    return axios.post("http://localhost:8080/api/employers/register", {
+      companyName,
+      emailAddress,
+      id,
+      password,
+      webAddress,
+    });
+  }
+
   getEmployers() {
     return axios.get("http://localhost:8080/api/employers/getall");
   }
