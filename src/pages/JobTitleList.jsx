4421807d0699a7866f19a76949a168cf77bdf83d
GeminusF <78738210+GeminusF@users.noreply.github.com>
2021-06-13 01:59:59 +0400

    JobTitle List UI changed

--- src/pages/JobTitleList.jsx
+++ src/pages/JobTitleList.jsx
@@ -1,41 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+import { Header } from "semantic-ui-react";
 import JobTitleService from "../services/jobTitleService";
 
-const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
-
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover,
-    },
-  },
-}))(TableRow);
-
-const useStyles = makeStyles({
-  table: {
-    minWidth: 700,
-  },
-});
-
 export default function JobTitleList() {
-  const classes = useStyles();
-
   const [jobtitles, setJobtitles] = useState([]);
 
   useEffect(() => {
@@ -46,25 +13,10 @@ export default function JobTitleList() {
   }, []);
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell align="right">Id</StyledTableCell>
-            <StyledTableCell align="right">Başlıq</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {jobtitles.map((jobtitle) => (
-            <StyledTableRow key={jobtitle.id}>
-              <StyledTableCell component="th" scope="row">
-                {jobtitle.id}
-              </StyledTableCell>
-              <StyledTableCell align="right">{jobtitle.title}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div>
+      {jobtitles.map((jobtitle) => (
+        <Header as="h2" icon="briefcase" content={jobtitle.title} />
+      ))}
+    </div>
   );
 }
