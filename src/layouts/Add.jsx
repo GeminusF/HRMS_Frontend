ed36acc6297bd07018ab27beb84f48ff5321c557
GeminusF <78738210+GeminusF@users.noreply.github.com>
2021-06-14 13:02:32 +0400

    New Layout: Add New page: HomePage

---
+++ src/layouts/Add.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useFormik } from 'formik';
+import JobVacancyForm from "../pages/JobVacancyForm";
+
+
+export default function Add() {
+  return (
+    <div>
+      <JobVacancyForm></JobVacancyForm>
+    </div>
+  );
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HomePage() {
+    return (
+        <div>
+            Figma design will be
+        </div>
+    )
+}
